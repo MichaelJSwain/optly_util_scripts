ba25cf04eac47e9a31052f0f24ff974b473c43c8
Michael Swain <michaelswain@tommy.com>
2025-02-17 15:33:20 +0100

    add audience IDs to projects in optimizelyProjects

--- optimizelyProjects.js
+++ optimizelyProjects.js
@@ -1,5 +1,12 @@
 require("dotenv").config();
-const { TH_PROJECT_ID, CK_PROJECT_ID } = process.env;
+const { TH_PROJECT_ID, 
+        CK_PROJECT_ID, 
+        TH_QA_AUDIENCE_ID,
+        TH_MB_AUDIENCE_ID,
+        TH_DT_AUDIENCE_ID,
+        CK_QA_AUDIENCE_ID,
+        CK_MB_AUDIENCE_ID,
+        CK_DT_AUDIENCE_ID  } = process.env;
 
 const optimizelyProjects = {
     th: [
@@ -7,7 +14,12 @@ const optimizelyProjects = {
               name: "TH",
               projectID: parseInt(TH_PROJECT_ID),
               defaultUrl: "(uk|nl|de|fr|it|es|pl).tommy.com",
-              editorUrl: "nl.tommy.com"
+              editorUrl: "nl.tommy.com",
+              audiences: {
+                qa: TH_QA_AUDIENCE_ID,
+                mb: TH_MB_AUDIENCE_ID,
+                dt: TH_DT_AUDIENCE_ID
+              }
           }
       ],
     ck: [
@@ -15,7 +27,12 @@ const optimizelyProjects = {
               name: "CK",
               projectID: parseInt(CK_PROJECT_ID),
               defaultUrl: "www.calvinklein.(co.uk|nl|de|fr|it|es|pl)",
-              editorUrl: "www.calvinklein.nl"
+              editorUrl: "www.calvinklein.nl",
+              audiences: {
+                qa: CK_QA_AUDIENCE_ID,
+                mb: CK_MB_AUDIENCE_ID,
+                dt: CK_DT_AUDIENCE_ID
+              }
           }
       ],
     db: [
@@ -23,13 +40,23 @@ const optimizelyProjects = {
             name: "TH",
             projectID: parseInt(TH_PROJECT_ID),
             defaultUrl: "(uk|nl|de|fr|it|es|pl).tommy.com",
-            editorUrl: "nl.tommy.com"
+            editorUrl: "nl.tommy.com",
+            audiences: {
+                qa: TH_QA_AUDIENCE_ID,
+                mb: TH_MB_AUDIENCE_ID,
+                dt: TH_DT_AUDIENCE_ID
+              }
         },
         {
           name: "CK",
           projectID: parseInt(CK_PROJECT_ID),
           defaultUrl: "www.calvinklein.(co.uk|nl|de|fr|it|es|pl)",
-          editorUrl: "www.calvinklein.nl"
+          editorUrl: "www.calvinklein.nl",
+          audiences: {
+            qa: CK_QA_AUDIENCE_ID,
+            mb: CK_MB_AUDIENCE_ID,
+            dt: CK_DT_AUDIENCE_ID
+          }
         }
       ]
   }
